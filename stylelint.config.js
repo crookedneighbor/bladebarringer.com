7a156c1c21376b3cd12e84c7d24b23086169bc92
Blade Barringer <blade@crookedneighbor.com>
2023-05-03 09:45:29 -0500

    chore: update vitejs/plugin-vue

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-config-html/vue",
-  ],
+  extends: ["stylelint-config-standard", "stylelint-config-html/vue"],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {},
