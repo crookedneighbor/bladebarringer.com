4347c049d566d2c5740c61dc21c80b85d3e0da7d
Blade Barringer <blade@crookedneighbor.com>
2023-10-25 16:59:04 -0500

    chore: snake the cat

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <snake-the-cat></snake-the-cat>
+  </div>
+</template>
+
+<script setup lang="ts">
+// TODO
+</script>
+
+<style>
+body {
+  @apply bg-yellow-300;
+}
+</style>
