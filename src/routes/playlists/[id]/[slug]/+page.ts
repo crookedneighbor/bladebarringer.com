590da3b0815d3afbdb6f498f0ce8b2cba82768ef
Blade Barringer <blade@crookedneighbor.com>
2024-12-25 16:54:57 -0600

    fix: stop hardcoding 2024

--- src/routes/playlists/[id]/[slug]/+page.ts
+++ src/routes/playlists/[id]/[slug]/+page.ts
@@ -2,7 +2,7 @@ import playlists from '$lib/playlist-data';
 import { error } from '@sveltejs/kit';
 
 export async function load({ params }) {
-	const playlist = playlists['2024'].tracks;
+	const playlist = playlists[params.id].tracks;
 	const track = playlist[params.slug];
 	if (!track) {
 		return error(404, 'Not Found');
