367fde57820d256fd63855bae3126ca83e8ce2ef
Blade Barringer <blade@crookedneighbor.com>
2024-12-25 17:01:23 -0600

    feat: don't re-add badncamp urls for tracks that already exist

--- scripts/populate-playlist.js
+++ scripts/populate-playlist.js
@@ -2,16 +2,20 @@ import 'dotenv/config';
 import { compile } from 'mdsvex';
 import { getPlaylist } from './spotify/playlist.js';
 import { lookupArtist } from './bandcamp/lookup-artist.js';
-import { writeFileSync, readdirSync, readFileSync } from 'node:fs';
+import { writeFileSync, readdirSync, readFileSync, existsSync } from 'node:fs';
 import { resolve as resolvePath } from 'node:path';
 
 const PLAYLIST_ID = process.env.PLAYLIST_ID;
 
-function populateBandcampLinks(playlist) {
+function populateBandcampLinks(playlist, previousEntries) {
 	let bandcampPromise = Promise.resolve();
 
 	Object.values(playlist.tracks).forEach((track) => {
 		bandcampPromise = bandcampPromise.then(async () => {
+			if (previousEntries && previousEntries.tracks[track.id]) {
+				// we've already looked up this track
+				track.bandcampPath = previousEntries.tracks[track.id].bandcampPath;
+			}
 			let { name, artist } = track;
 			name = name.toLowerCase();
 			artist = artist.split(' | ')[0].toLowerCase();
@@ -48,11 +52,11 @@ async function populatePageDescriptions(playlist) {
 
 async function populatePlaylistPage(id) {
 	const slug = process.env.PLAYLIST_SLUG;
-	const playlist = await getPlaylist(id, slug);
-	await populateBandcampLinks(playlist);
-	await populatePageDescriptions(playlist);
-
 	const path = resolvePath('src', 'lib', 'playlist-data', 'raw', `${slug}.json`);
+	const previousEntries = existsSync(path) && JSON.parse(readFileSync(path, 'utf8'));
+	const playlist = await getPlaylist(id, slug);
+	await populateBandcampLinks(playlist, previousEntries);
+	await populatePageDescriptions(playlist);
 
 	writeFileSync(path, JSON.stringify(playlist, null, 2), 'utf8');
 
