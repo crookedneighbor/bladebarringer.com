49fbe1fdac7abf138d657443df8f12abd0a76015
Blade Barringer <blade@crookedneighbor.com>
2024-12-27 15:24:29 -0600

    feat: memoize access token

--- scripts/spotify/access-token.js
+++ scripts/spotify/access-token.js
@@ -3,15 +3,20 @@ import 'dotenv/config';
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-export async function getAccessToken() {
-	const { access_token } = await fetch('https://accounts.spotify.com/api/token', {
-		method: 'POST',
-		body:
-			'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET,
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		}
-	}).then((res) => res.json());
+let accessTokenPromise;
 
-	return access_token;
+export async function getAccessToken() {
+	if (!accessTokenPromise) {
+		accessTokenPromise = fetch('https://accounts.spotify.com/api/token', {
+			method: 'POST',
+			body:
+				'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET,
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		})
+			.then((res) => res.json())
+			.then((d) => d.access_token);
+	}
+	return accessTokenPromise;
 }
