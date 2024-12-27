747a98f4362e4325ff7a185cf69f69237bd8f96d
Blade Barringer <blade@crookedneighbor.com>
2024-12-26 20:45:07 -0600

    feat: add track metadata to svx files

--- scripts/populate-playlist.js
+++ scripts/populate-playlist.js
@@ -3,33 +3,9 @@ import { getPlaylist } from './spotify/playlist.js';
 import { lookupArtist } from './bandcamp/lookup-artist.js';
 import { writeFileSync, readFileSync, existsSync } from 'node:fs';
 import { resolve as resolvePath } from 'node:path';
+import { compile } from 'mdsvex';
 
 const PLAYLIST_ID = process.env.PLAYLIST_ID;
-
-function populateBandcampLinks(playlist, previousEntries) {
-	let bandcampPromise = Promise.resolve();
-
-	Object.values(playlist.tracks).forEach((track) => {
-		bandcampPromise = bandcampPromise.then(async () => {
-			if (previousEntries && previousEntries.tracks[track.id]) {
-				// we've already looked up this track
-				track.bandcampPath = previousEntries.tracks[track.id].bandcampPath;
-				return;
-			}
-			let { name, artist } = track;
-			name = name.toLowerCase();
-			artist = artist.split(' | ')[0].toLowerCase();
-
-			const link = await lookupArtist(artist, name);
-
-			if (link) {
-				track.bandcampPath = link;
-			}
-		});
-	});
-
-	return bandcampPromise;
-}
 
 async function populatePageDescriptions(playlist) {
 	const { slug } = playlist;
@@ -37,9 +13,44 @@ async function populatePageDescriptions(playlist) {
 	await Promise.all(
 		Object.values(playlist.tracks).map(async (track) => {
 			const pathToFile = resolvePath(path, `${track.id}.svx`);
-			if (!existsSync(pathToFile)) {
-				writeFileSync(pathToFile, 'TODO', 'utf-8');
+			let pageData = `
+TODO`;
+			if (existsSync(pathToFile)) {
+				const existingFileData = readFileSync(pathToFile, 'utf-8');
+				pageData = existingFileData.split('---\n').at(-1);
+				if (!existingFileData.startsWith('---')) {
+					pageData = `\n${pageData}`;
+				}
+				const { data } = await compile(existingFileData);
+				// overwrite the fields if they already exist
+				if (data.fm?.bandcampPath) {
+					track.bandcampPath = data.fm.bandcampPath;
+				}
+			} else {
+				let { name, artist } = track;
+				name = name.toLowerCase();
+				artist = artist.split(' | ')[0].toLowerCase();
+				const link = await lookupArtist(artist, name);
+
+				if (link) {
+					track.bandcampPath = link;
+				}
 			}
+			const metadata = Object.keys(track).reduce((str, key) => {
+				if (str) {
+					str += '\n';
+				}
+				str += `${key}: ${track[key]}`;
+				return str;
+			}, '');
+			writeFileSync(
+				pathToFile,
+				`---
+${metadata}
+---
+${pageData}`,
+				'utf-8'
+			);
 		})
 	);
 }
@@ -47,9 +58,7 @@ async function populatePageDescriptions(playlist) {
 async function populatePlaylistPage(id) {
 	const slug = process.env.PLAYLIST_SLUG;
 	const path = resolvePath('src', 'lib', 'playlist-data', 'raw', `${slug}.json`);
-	const previousEntries = existsSync(path) && JSON.parse(readFileSync(path, 'utf8'));
 	const playlist = await getPlaylist(id, slug);
-	await populateBandcampLinks(playlist, previousEntries);
 	await populatePageDescriptions(playlist);
 
 	writeFileSync(path, JSON.stringify(playlist, null, 2), 'utf8');
