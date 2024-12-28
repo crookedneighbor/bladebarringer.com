eca0767c42d11b4bb2af07547ae6782dd954dea5
Blade Barringer <blade@crookedneighbor.com>
2024-12-27 19:10:05 -0600

    feat: update height

--- src/lib/components/SpotifyPlayer/SpotifyPlayer.svelte
+++ src/lib/components/SpotifyPlayer/SpotifyPlayer.svelte
@@ -44,7 +44,7 @@
 		style="border-radius:12px"
 		src={`https://open.spotify.com/embed/playlist/${id}?utm_source=generator&theme=0`}
 		width="100%"
-		height="152"
+		height="352"
 		frameBorder="0"
 		allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
 		loading="lazy"
