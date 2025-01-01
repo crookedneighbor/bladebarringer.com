f654ec55ed50357e0be11455662cc123a4e6f227
Blade Barringer <blade@crookedneighbor.com>
2025-01-01 15:48:18 -0600

    feat: add current track

---
+++ src/lib/components/SpotifyPlayer/CurrentTrack.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+	import type { Track } from './types';
+
+	interface Props {
+		currentTrack?: Track;
+	}
+	let { currentTrack }: Props = $props();
+</script>
+
+{#if currentTrack}
+	{#key currentTrack.id}
+		<div transition:slide class="mx-2 text-xl">
+			{currentTrack.number}. {currentTrack.name}
+			<div class="text-xs text-gray-500">{currentTrack.artist}</div>
+		</div>
+	{/key}
+{/if}
