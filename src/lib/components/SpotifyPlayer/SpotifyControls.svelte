1875d681e10f3ba192163892b2d3984d5b36553b
Blade Barringer <blade@crookedneighbor.com>
2025-01-01 15:03:24 -0600

    feat: add spotify controls

---
+++ src/lib/components/SpotifyPlayer/SpotifyControls.svelte
@@ -0,0 +1,132 @@
+<script lang="ts" module>
+	export interface Props {
+		previousTrackID: string;
+		nextTrackID: string;
+		prevDisabled: boolean;
+		onTrackChange: (newID: string) => void;
+	}
+</script>
+
+<script lang="ts">
+	import { player } from '$lib/components/HeadlessSpotifyController/HeadlessSpotifyController.svelte';
+
+	let { previousTrackID, nextTrackID, prevDisabled, onTrackChange }: Props = $props();
+</script>
+
+<button
+	disabled={prevDisabled}
+	class="-mb-4"
+	class:text-gray-400={prevDisabled}
+	onclick={() => {
+		if (player.position > 3000) {
+			player.restart();
+			return;
+		}
+
+		player.load(previousTrackID);
+		onTrackChange(previousTrackID);
+
+		if (player.autoplay) {
+			player.play();
+		}
+	}}
+>
+	<span class="sr-only">Previous track</span>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="size-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689v8.122ZM11.25 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977v8.122Z"
+		/>
+	</svg>
+</button>
+
+{#if player.playing}
+	<button
+		class="-mb-6 bg-white rounded-full z-10"
+		onclick={() => {
+			player.autoplay = false;
+			player.toggle();
+		}}
+	>
+		<span class="sr-only">Pause</span>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="size-16"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M14.25 9v6m-4.5 0V9M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+			/>
+		</svg>
+	</button>
+{:else}
+	<button
+		class="-mb-6 bg-white rounded-full z-10"
+		onclick={() => {
+			player.autoplay = true;
+			player.toggle();
+		}}
+	>
+		<span class="sr-only">Play</span>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="size-16"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+			/>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
+			/>
+		</svg>
+	</button>
+{/if}
+
+<button
+	class="-mb-4"
+	onclick={() => {
+		player.load(nextTrackID);
+		onTrackChange(nextTrackID);
+
+		if (player.autoplay) {
+			player.play();
+		}
+	}}
+>
+	<span class="sr-only">Next track</span>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="size-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z"
+		/>
+	</svg>
+</button>
---
+++ src/lib/components/SpotifyPlayer/SpotifyControls.test.svelte.ts
@@ -0,0 +1,162 @@
+import { act, render, screen } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
+import SpotifyControls, { type Props } from './SpotifyControls.svelte';
+import {
+	player,
+	resetForTest
+} from '$lib/components/HeadlessSpotifyController/HeadlessSpotifyController.svelte';
+
+describe('SpotifyControls', () => {
+	let props: Props;
+
+	beforeEach(() => {
+		props = {
+			previousTrackID: 'prev',
+			nextTrackID: 'next',
+			prevDisabled: false,
+			onTrackChange: vi.fn()
+		};
+
+		vi.spyOn(player, 'restart');
+		vi.spyOn(player, 'load');
+		vi.spyOn(player, 'play');
+		vi.spyOn(player, 'toggle');
+	});
+
+	afterEach(() => {
+		resetForTest();
+	});
+
+	describe('previous button', () => {
+		it('loads the previous track', async () => {
+			const user = userEvent.setup();
+			render(SpotifyControls, props);
+
+			await user.click(screen.getByText('Previous track'));
+
+			expect(player.load).toBeCalledTimes(1);
+			expect(player.load).toBeCalledWith('prev');
+			expect(props.onTrackChange).toBeCalledTimes(1);
+			expect(props.onTrackChange).toBeCalledWith('prev');
+		});
+
+		it('restarts player when position is greater than 3000', async () => {
+			const user = userEvent.setup();
+			render(SpotifyControls, props);
+
+			player.position = 3001;
+			await user.click(screen.getByText('Previous track'));
+
+			expect(player.load).not.toBeCalled();
+			expect(player.restart).toBeCalledTimes(1);
+			expect(props.onTrackChange).not.toBeCalled();
+
+			vi.mocked(player.restart).mockReset();
+			player.position = 3000;
+			await user.click(screen.getByText('Previous track'));
+
+			expect(player.load).toBeCalledTimes(1);
+			expect(props.onTrackChange).toBeCalledTimes(1);
+			expect(player.restart).not.toBeCalled();
+		});
+
+		it('autoplays if autoplayer is on', async () => {
+			const user = userEvent.setup();
+			render(SpotifyControls, props);
+
+			player.autoplay = false;
+			await user.click(screen.getByText('Previous track'));
+			expect(player.play).not.toBeCalled();
+
+			player.autoplay = true;
+			await user.click(screen.getByText('Previous track'));
+			expect(player.play).toBeCalledTimes(1);
+		});
+	});
+
+	describe('next button', () => {
+		it('loads the next track', async () => {
+			const user = userEvent.setup();
+			render(SpotifyControls, props);
+
+			await user.click(screen.getByText('Next track'));
+
+			expect(player.load).toBeCalledTimes(1);
+			expect(player.load).toBeCalledWith('next');
+			expect(props.onTrackChange).toBeCalledTimes(1);
+			expect(props.onTrackChange).toBeCalledWith('next');
+		});
+
+		it('autoplays if autoplayer is on', async () => {
+			const user = userEvent.setup();
+			render(SpotifyControls, props);
+
+			player.autoplay = false;
+			await user.click(screen.getByText('Next track'));
+			expect(player.play).not.toBeCalled();
+
+			player.autoplay = true;
+			await user.click(screen.getByText('Next track'));
+			expect(player.play).toBeCalledTimes(1);
+		});
+	});
+
+	describe('play/pause button', () => {
+		it('toggles when pressed', async () => {
+			const user = userEvent.setup();
+			render(SpotifyControls, props);
+
+			await user.click(screen.getByText('Play'));
+			await act(() => (player.playing = true));
+			await user.click(screen.getByText('Pause'));
+
+			expect(player.toggle).toBeCalledTimes(2);
+		});
+
+		it('renders play button when currently paused', async () => {
+			await act(() => (player.playing = false));
+
+			render(SpotifyControls, props);
+
+			expect(screen.queryByText('Play')).toBeInTheDocument();
+			expect(screen.queryByText('Pause')).not.toBeInTheDocument();
+		});
+
+		it('renders pause button when currently playing', async () => {
+			await act(() => (player.playing = true));
+
+			render(SpotifyControls, props);
+
+			expect(screen.queryByText('Pause')).toBeInTheDocument();
+			expect(screen.queryByText('Play')).not.toBeInTheDocument();
+		});
+
+		it('turns autoplay on when playing', async () => {
+			const user = userEvent.setup();
+			await act(() => {
+				player.playing = false;
+				player.autoplay = false;
+			});
+
+			render(SpotifyControls, props);
+
+			await user.click(screen.getByText('Play'));
+
+			expect(player.autoplay).toEqual(true);
+		});
+
+		it('turns autoplay off when pausing', async () => {
+			const user = userEvent.setup();
+			await act(() => {
+				player.playing = true;
+				player.autoplay = true;
+			});
+
+			render(SpotifyControls, props);
+
+			await user.click(screen.getByText('Pause'));
+
+			expect(player.autoplay).toEqual(false);
+		});
+	});
+});
