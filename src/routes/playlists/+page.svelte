4506e65cc4f09be28f525d9a5583a63ba268cb6b
Blade Barringer <blade@crookedneighbor.com>
2025-01-07 23:44:39 -0600

    fix: style playlists page

--- src/routes/playlists/+page.svelte
+++ src/routes/playlists/+page.svelte
@@ -3,10 +3,19 @@
 	let { playlists } = $derived(data);
 </script>
 
-<ul>
-	{#each playlists as playlist}
-		<li>
-			<a href={`/playlists/${playlist.slug}`}><img src={playlist.image} alt={playlist.name} /></a>
-		</li>
-	{/each}
-</ul>
+<div class="container m-auto py-4">
+	<div class="px-4">
+		<h1 class="text-8xl">Playlists</h1>
+		<hr />
+		<h2 class="text-4xl my-2">Year Playlists</h2>
+	</div>
+	<ul class="flex w-full">
+		{#each playlists as playlist}
+			<li class="w-1/3 p-4">
+				<a href={`/playlists/${playlist.slug}`}
+					><img class="w-full" src={playlist.image} alt={playlist.name} /></a
+				>
+			</li>
+		{/each}
+	</ul>
+</div>
