2e081caf4d557677688c835be285092ee646ebbe
Blade Barringer <blade@crookedneighbor.com>
2025-01-12 10:05:09 -0600

    fix: correct domain name in share urls

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,24 @@
 	import { page } from '$app/state';
 	import '../app.css';
 	let { children } = $props();
+
+	let shareURL = $derived.by(() => {
+		let pathName = '';
+
+		if (page.data.og.url) {
+			pathName = page.data.og.url;
+		} else if (page.url.pathname !== '/') {
+			pathName = page.url.pathname;
+		}
+
+		return `https://bladebarringer.com${pathName}`;
+	});
 </script>
 
 <svelte:head>
 	<meta property="og:title" content={page.data.og.title} />
 	<meta property="og:type" content={page.data.og.type || 'website'} />
-	<meta property="og:url" content={page.data.og.url || page.url.href} />
+	<meta property="og:url" content={shareURL} />
 	<meta property="og:image" content={page.data.og.image} />
 	<meta property="og:description" content={page.data.og.description} />
 	<meta property="og:logo" content={page.data.og.logo || '/favicon.png'} />
