5cacb2c92e19cd8cde0be074cf8a5e3154b8edd3
Blade Barringer <blade@crookedneighbor.com>
2025-01-18 14:16:05 -0600

    refactor: make it easier to add more playlist groups

--- src/routes/playlists/+page.svelte
+++ src/routes/playlists/+page.svelte
@@ -4,10 +4,8 @@
 </script>
 
 <div class="container m-auto py-4">
-	<div class="px-4">
-		<h1 class="text-8xl">Playlists</h1>
-		<h2 class="text-4xl my-2">Year Playlists</h2>
-	</div>
+	<h1 class="text-8xl">Playlists</h1>
+	<h2>Year Playlists</h2>
 	<ul class="flex w-full">
 		{#each playlists as playlist}
 			<li class="w-full sm:w-1/3 p-4">
@@ -23,3 +21,14 @@
 		{/each}
 	</ul>
 </div>
+
+<style lang="postcss">
+	h1,
+	h2 {
+		@apply px-4;
+	}
+
+	h2 {
+		@apply text-4xl my-2;
+	}
+</style>
